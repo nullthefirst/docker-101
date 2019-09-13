a492a4cd3b1f89e470e1908de21aa47fe1b05c7e
Usheninte <jrandangana@gmail.com>
2019-09-13 09:51:37 +0000

    spaced syntax

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.get('/', (req, res) => {
     <h1>Home</h1>
     <p>Docker is awesome!</p>
     <a href="/more" alt="Next Page">Next Page</a>
-    `
-  )
+    `,
+  );
 });
 
 app.get('/more', (req, res) => {
@@ -21,8 +21,8 @@ app.get('/more', (req, res) => {
     <h1>Page Two</h1>
     <p>Node.js is pretty great too!!</p>
     <a href="/" alt="Back Home">Back Home</a>
-    `
-  )
+    `,
+  );
 });
 
 app.listen(PORT, HOST);
